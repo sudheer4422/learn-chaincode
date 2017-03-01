a38e248c3b3625337d33a775a51c7f1543c5a243
sudheer4422 <sudheer4422@gmail.com>
2017-03-01 15:09:08 +0530

    shopping cart

--- finished/chaincode_finished.go
+++ finished/chaincode_finished.go
@@ -1,89 +1,118 @@
 package main
 
 import (
-	
-	"fmt"
 	"errors"
-    "github.com/hyperledger/fabric/core/chaincode/shim"
+	"fmt"
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+)
+
+var logger = shim.NewLogger("Shopping_Cart")
+
+type ShoppingCart struct {
+	
+}
+
+const (
+	userTable = "UserTable"
+	productTable        = "ProductTable"
 )
 
 
-type SimpleChaincode struct {
-}
-
-func main(){
+func (t *ShoppingCart) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error){
 	
-	err := shim.Start(new(SimpleChaincode))
-	if err != nil {
-		fmt.Printf("sudheer")
-	}	
-}
-
-
-func(t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	if len(args) != 1 {
-		return nil, errors.New("expected 1 argument in INIT method")
-	}
-	err := stub.PutState("hello world", []byte(args[0]))
-	if err != nil {
-		 return nil, err
-	}
-return nil, nil
-}
-
-
-func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error){
-	
-	if function == "init"{
-		return t.Init(stub, "init", args)
-	}
-	if function == "write"{
-		return t.write(stub, args)
-	}
-	return nil, nil
-}
-
-
-func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error){
-	
-	if function == "read"{
-		t.read(stub, args)
-	}
-	return nil, nil
-}
-
-
-func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface,args []string) ([]byte, error){
-	
-	var key,jsonRESP string
 	var err error
 	
-	if len(args) !=1 {
-		
-	} 
-	
-	key = args[0]
-	valAsBytes, err := stub.GetState(key)
-	if err != nil {
-		jsonRESP = "{\"Error\":\"Failed to get state for " + key + "\"}"
-		return nil, errors.New(jsonRESP)
+	if len(args) > 0 {
+		logger.Error("Incorrect number of arguments")
+		return nil, errors.New("Incorrect number of arguments. No arguments required.")
 	}
-	return valAsBytes, nil
+	
+	_, err = stub.GetTable(userTable)
+	if err == shim.ErrTableNotFound {
+		err = stub.CreateTable(userTable, []*shim.ColumnDefinition{
+			&shim.ColumnDefinition{Name: "UserId", Type: shim.ColumnDefinition_STRING, Key: true},
+			&shim.ColumnDefinition{Name: "UserName", Type: shim.ColumnDefinition_STRING, Key: false},
+			&shim.ColumnDefinition{Name: "Type", Type: shim.ColumnDefinition_STRING, Key: false},
+		})
+		if err != nil {
+			logger.Errorf("Error creating table:%s - %s", userTable, err.Error())
+			return nil, errors.New("Failed creating userTable table.")
+		}
+}else {
+		logger.Info("Table already exists")
+	}
+
+    _, err = stub.GetTable(productTable)
+    if err == shim.ErrTableNotFound {
+		err = stub.CreateTable(productTable, []*shim.ColumnDefinition{
+			&shim.ColumnDefinition{Name: "ProductId", Type: shim.ColumnDefinition_STRING, Key: true},
+			&shim.ColumnDefinition{Name: "ProductName", Type: shim.ColumnDefinition_STRING, Key: true},
+		})
+		if err != nil {
+			logger.Errorf("Error creating table:%s - %s", productTable, err.Error())
+			return nil, errors.New("Failed creating productTable .")
+		}
+	} else {
+		logger.Info("Table already exists")
+	}
+
+logger.Info("Successfully deployed chain code")
+
+	return nil, nil
+}
+	
+func (t *ShoppingCart) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+
+	if function == "enroll" {
+		return t.enroll(stub, args)
+	}
+
+	logger.Errorf("Unimplemented method :%s called", function)
+
+	return nil, errors.New("Unimplemented '" + function + "' invoked")
 }
 
-func (t *SimpleChaincode) write(stub shim.ChaincodeStubInterface, args []string) ([]byte, error){
-	var key, value string
-	var err error
+func (t *ShoppingCart) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	
-	if len(args)!=2{
-		return nil, errors.New("2 arg expected in write method")
+	return nil, errors.New("Invalid query function name")
+}
+
+
+func (t *ShoppingCart) enroll(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	logger.Info("In enroll function")
+	if len(args) < 3 {
+		logger.Error("Incorrect number of arguments")
+		return nil, errors.New("Incorrect number of arguments. Specify device id, public key, owner, owner for check1, check2 and check3.")
 	}
+
+	UserId := args[0]
+	UserName := args[1]
+	Type := args[2]
 	
-	key = args[0]
-	value = args[1]
-    err = stub.PutState(key, []byte(value))
-    if err != nil {
-		return nil, err
+	ok, err := stub.InsertRow(userTable, shim.Row{
+		Columns: []*shim.Column{
+			&shim.Column{Value: &shim.Column_String_{String_: UserId}},
+			&shim.Column{Value: &shim.Column_String_{String_: UserName}},
+			&shim.Column{Value: &shim.Column_String_{String_: Type}},
+		},
+	})
+
+	if !ok || err != nil {
+		logger.Errorf("Error in enrolling a new user:%s", err)
+		return nil, errors.New("Error in enrolling a new user")
 	}
+	logger.Infof("Enrolled device %s", UserId)
+
 	return nil, nil
+}
+
+
+
+func main() {
+	
+	logger.SetLevel(shim.LogDebug)
+	err := shim.Start(new(ShoppingCart))
+	if err != nil {
+		fmt.Printf("Error starting Energy trading chaincode: %s", err)
+	}
 }
